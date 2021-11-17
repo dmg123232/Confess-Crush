6207b6bd4694d1ef8e52ba78ae0df3ad45dc34a6
PHUOBG <88281173+PHUOBG@users.noreply.github.com>
2021-11-17 10:06:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "tớ mún nói đìu ni..........",
-    introTitle: 'bạn nii ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "phương mún nói đìu ni..........",
+    introTitle: 'bạn niiiiiiiiii ',
+    introDesc: `cậu quá là cutee,đẹp trai,cậu chính là gu mình đóa,i lác you pặc pặc:333333`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng cậu đã say tớ ngay từ lúc thấy nụ cười ấy:333 ',
+    btnYes: 'Thích nắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu phương dậy <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'chìu nay 3h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun,iu phương love you <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
